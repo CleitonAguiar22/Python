c292a066dec87c1ddbdb014d59ba1c111d65d2db
Cleiton <cleitonmik2022@gmail.com>
2025-04-08 10:51:31 -0300

    Add files via upload

---
+++ Funções/Funções, Repetição/index08.py
@@ -0,0 +1,20 @@
+frutas = ["maĂ§ĂŁ", "banana", "tomate", "laranja"]
+
+# A
+
+print(frutas[1], frutas[3])
+
+# B
+
+frutas.pop(0)
+frutas.pop(1)
+print(frutas)
+
+# C
+
+frutas.append("mamĂŁo")
+print(frutas)
+
+# D
+
+print(len(frutas))
